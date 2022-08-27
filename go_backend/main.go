1acb80c19041f85d42c8663865c20d8f7e0868bb
TerrificTable <85793326+TerrificTable@users.noreply.github.com>
2022-08-27 23:47:39 +0200

    load project in GoLand

--- go_backend/main.go
+++ go_backend/main.go
@@ -3,38 +3,65 @@ package main
 import (
     "fmt"
     mqtt "github.com/eclipse/paho.mqtt.golang"
+    "time"
 )
 
+
+
+var connected bool = false
+
+
+
+
 func messagePubHandler(client mqtt.Client, msg mqtt.Message) {
-	fmt.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
+	fmt.Println(fmt.Sprintf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic()))
 }
 
 func connectHandler(client mqtt.Client) {
+    connected = true
 	fmt.Println("Connected")
 }
 
 func connectLostHandler(client mqtt.Client, err error) {
-	fmt.Printf("Connect lost: %v", err)
+	fmt.Println(fmt.Sprintf("Connect lost: %v", err))
+    connected = false
 }
 
 
-func main() {
 
-	broker 	:= "localhost"
-    port 	:= 8883
-
-
+func connect(broker string, port int) {
     opts := mqtt.NewClientOptions()
     opts.AddBroker(fmt.Sprintf("tcp://%s:%d", broker, port))
     opts.SetClientID("/")
-    // opts.SetUsername("mqtt")
-    // opts.SetPassword("mqtt")
+    opts.SetUsername("mqtt")
+    opts.SetPassword("mqtt")
     opts.SetDefaultPublishHandler(messagePubHandler)
     opts.OnConnect = connectHandler
     opts.OnConnectionLost = connectLostHandler
     client := mqtt.NewClient(opts)
     if token := client.Connect(); token.Wait() && token.Error() != nil {
         panic(token.Error())
-	}
+    }
+    return client
+}
+
+
+func main() {
+
+    fmt.Println("Starting...")
+
+	broker 	    := "mosquitto"
+    port 	    := 8883
+
+    client := connect(broker, port);
+    
+    time.Sleep(2 * time.Second)
+
+
+    if (connected) {
+        for {} // infinite "`while`" loop
+    }
+
+    fmt.Println("Exiting...")
 
 }
