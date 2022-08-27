883a495929e69c4a6565aa9967dc1b728a4de09c
TerrificTable <85793326+TerrificTable@users.noreply.github.com>
2022-08-27 23:49:46 +0200

    moved connect into its own function

--- go_backend/main.go
+++ go_backend/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-    "fmt"
-    mqtt "github.com/eclipse/paho.mqtt.golang"
-    "time"
+	"fmt"
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"time"
 )
 
 
@@ -13,55 +13,53 @@ var connected bool = false
 
 
 
+
+
+
 func messagePubHandler(client mqtt.Client, msg mqtt.Message) {
 	fmt.Println(fmt.Sprintf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic()))
 }
 
 func connectHandler(client mqtt.Client) {
-    connected = true
+	connected = true
 	fmt.Println("Connected")
 }
 
 func connectLostHandler(client mqtt.Client, err error) {
+	connected = false
 	fmt.Println(fmt.Sprintf("Connect lost: %v", err))
-    connected = false
 }
 
-
-
-func connect(broker string, port int) {
-    opts := mqtt.NewClientOptions()
-    opts.AddBroker(fmt.Sprintf("tcp://%s:%d", broker, port))
-    opts.SetClientID("/")
-    opts.SetUsername("mqtt")
-    opts.SetPassword("mqtt")
-    opts.SetDefaultPublishHandler(messagePubHandler)
-    opts.OnConnect = connectHandler
-    opts.OnConnectionLost = connectLostHandler
-    client := mqtt.NewClient(opts)
-    if token := client.Connect(); token.Wait() && token.Error() != nil {
-        panic(token.Error())
-    }
-    return client
+func connect(broker string, port int) mqtt.Client {
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(fmt.Sprintf("tcp://%s:%d", broker, port))
+	opts.SetClientID("/")
+	opts.SetUsername("mqtt")
+	opts.SetPassword("mqtt")
+	opts.SetDefaultPublishHandler(messagePubHandler)
+	opts.OnConnect = connectHandler
+	opts.OnConnectionLost = connectLostHandler
+	client := mqtt.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+	return client
 }
-
 
 func main() {
 
-    fmt.Println("Starting...")
+	fmt.Println("Starting...")
 
-	broker 	    := "mosquitto"
-    port 	    := 8883
+	broker 	:= "mosquitto"
+	port 	:= 8883
 
-    client := connect(broker, port);
-    
-    time.Sleep(2 * time.Second)
+	client := connect(broker, port)
+	time.Sleep(2 * time.Second)
 
+	if connected {
+		select {} // infinite "`while`" loop
+	}
 
-    if (connected) {
-        for {} // infinite "`while`" loop
-    }
-
-    fmt.Println("Exiting...")
+	fmt.Println("Exiting...")
 
 }
