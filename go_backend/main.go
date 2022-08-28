3958f56bd347e48ac260250d94e2786a786f185b
TerrificTable <85793326+TerrificTable@users.noreply.github.com>
2022-08-28 14:36:34 +0200

    connect to database and post data "recieved" from mqtt

--- go_backend/main.go
+++ go_backend/main.go
@@ -2,27 +2,31 @@ package main
 
 import (
 	"fmt"
-	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	_ "github.com/go-sql-driver/mysql"
+	"database/sql"
 )
 
 
 
 var connected bool = false
+var database *sql.DB
 
 
 
 
-
-
+// ============ MQTT ==============
 
 func messagePubHandler(client mqtt.Client, msg mqtt.Message) {
 	fmt.Println(fmt.Sprintf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic()))
+	querrySQL(database, "INSERT INTO Input(pin, voltage) VALUES(\"D1\", 1.5)")
 }
 
 func connectHandler(client mqtt.Client) {
 	connected = true
-	fmt.Println("Connected")
+	fmt.Println("MQTT Connected")
 }
 
 func connectLostHandler(client mqtt.Client, err error) {
@@ -30,13 +34,21 @@ func connectLostHandler(client mqtt.Client, err error) {
 	fmt.Println(fmt.Sprintf("Connect lost: %v", err))
 }
 
-func connect(broker string, port int) mqtt.Client {
+func messageReceiveHandler(client mqtt.Client, msg mqtt.Message) {
+	fmt.Printf("Topic '#/%s' logged...\n", msg.Topic())
+	querrySQL(database, "INSERT INTO Input(pin, voltage) VALUES(\"D1\", 1.5)")
+}
+
+
+
+
+func connectMQTT(broker string, port int) mqtt.Client {
 	opts := mqtt.NewClientOptions()
 	opts.AddBroker(fmt.Sprintf("tcp://%s:%d", broker, port))
 	opts.SetClientID("/")
 	opts.SetUsername("mqtt")
 	opts.SetPassword("mqtt")
-	opts.SetDefaultPublishHandler(messagePubHandler)
+	// opts.SetDefaultPublishHandler(messagePubHandler)
 	opts.OnConnect = connectHandler
 	opts.OnConnectionLost = connectLostHandler
 	client := mqtt.NewClient(opts)
@@ -46,6 +58,45 @@ func connect(broker string, port int) mqtt.Client {
 	return client
 }
 
+func subscribeTopic(client mqtt.Client, topic string, handler mqtt.MessageHandler) {
+	if token := client.Subscribe(topic, 0, handler); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+}
+
+// ============ MQTT ==============
+
+
+// ============ MYSQL ==============
+
+func connectDB(driverName string, dataSourceName string) *sql.DB {
+	db, err := sql.Open(driverName, dataSourceName)
+	if err != nil {
+		panic(err)
+	}
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(5)
+	fmt.Println("DB Connected")
+	return db
+}
+
+func querrySQL(db *sql.DB, sql string) {
+	for i := 0; i < 100; i++ {
+		go func(I int) {
+			rows, err := db.Query(sql)
+			if err != nil {
+				panic(err)
+			}
+			_ = rows.Close()
+			fmt.Println(I)
+		}(i)
+	}
+}
+
+// ============ MYSQL ==============
+
+
+
 func main() {
 
 	fmt.Println("Starting...")
@@ -53,11 +104,18 @@ func main() {
 	broker 	:= "mosquitto"
 	port 	:= 8883
 
-	client := connect(broker, port)
+	client := connectMQTT(broker, port)
 	time.Sleep(2 * time.Second)
 
+	database = connectDB("mysql", "root:mysql-pass@tcp(database:3306)/VoltsInput")
+	time.Sleep(2 * time.Second)
+
+	subscribeTopic(client, "#", messagePubHandler)
+
 	if connected {
-		select {} // infinite "`while`" loop
+		for {
+			time.Sleep(time.Second)
+		}
 	}
 
 	fmt.Println("Exiting...")
