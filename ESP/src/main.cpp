388b7afbcbb9a4d70532af18eedbd3b788c90ff3
TerrificTable <85793326+TerrificTable@users.noreply.github.com>
2022-09-04 01:22:38 +0200

    changed pins

--- ESP/src/main.cpp
+++ ESP/src/main.cpp
@@ -1,102 +1,134 @@
 #include <Arduino.h>
 
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
 #include "wifi.hpp"
 
 
-#define ARRAY_LENGTH(arr)       (sizeof(arr) / sizeof(arr[0]))
 
 
-// Input Pins
-#define D0  16
-#define D1  5
-#define D2  4
-#define D5  14
-#define D6  12
-#define D7  13
-#define D8  15
-// Status LED Pins
-#define D3  0
-#define D4  2
+const int ADC_pin = A0;
+int reading = 0;
+float analog_voltage;
+float d0_volt;
+float d5_volt;
+float d6_volt;
+float d7_volt;
 
 
-int _pins[] = {
-    D0,
-    D1,
-    D2,
-    D5,
-    D6,
-    D7,
-    D8
-};
+#define SDA     D2
+#define SCL     D1
 
-String pins[] = {
-    "D0",
-    "D1",
-    "D2",
-    "D5",
-    "D6",
-    "D7",
-    "D8"
-};
+// measure
+#define D0      16
+#define D5      14
+#define D6      12
+#define D7      13
+// led
+#define D3      0
+
+
+typedef struct {
+    int width         = 128;
+    int height        = 64;
+    int RESET         = 0;
+    int cursorPos     = 0;
+} Screen;
+Screen screen;
+
+
+Adafruit_SSD1306 display = Adafruit_SSD1306(screen.width, screen.height, &Wire, screen.RESET);
+
+
+
+
+
+void DisplayText(
+    String text, 
+    int positionY,
+    boolean clear   = false,
+    int positionX   = 0, 
+    int size        = 2, 
+    int color       = SSD1306_WHITE 
+) {
+    if (clear)  {
+        display.clearDisplay();
+        display.display();
+    }
+
+    display.setTextColor(color);
+    display.setTextSize(size);
+    display.setCursor(positionX, positionY);
+    display.println(text);
+    display.display();
+}
+
 
 
 void setup() {
-    Wire.begin();
     Serial.begin(115200);
-    delay(100);
+    Serial.println("Starting");
 
-    Serial.println("Starting...");
+    display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+    Serial.println("Screen Initialized");
+    display.clearDisplay();
 
-    pinMode(LED_BUILTIN, OUTPUT);
-    pinMode(D3, OUTPUT);
-    pinMode(D4, OUTPUT);
-    for (int i=0; i < ARRAY_LENGTH(pins); i++) {
-        pinMode(_pins[i], INPUT);
-        digitalWrite(_pins[i], LOW);
-    }
-
-    digitalWrite(LED_BUILTIN, 0);
     digitalWrite(D3, 1);
-    digitalWrite(D4, 1);
+
+    Serial.println("=============================");
 }
-
-
-float calculateVoltageInput(int inputValue) {
-    return (inputValue * 3.3) / 1024.0;
-}
-
-
-// on my "ESP8266 D1 Mini Lite"
-// "D1" and "D2"
-// are at 3.3V when they arnt connected to "GND"
 
 void loop() {
     WLAN();
     MQTT();
 
+
     if (States.WLANConnected) {
-        digitalWrite(D3, 1);
-        digitalWrite(D4, 0);
-
-        for (int i=0; i < ARRAY_LENGTH(pins); i++) {
-            String pin = pins[i];
-
-
-            int d1_inp_value = analogRead(_pins[i]);
-            float voltage = calculateVoltageInput(d1_inp_value);
-
-            Serial.print(pin + ": ");
-            Serial.println(voltage);
-            sendMQTT("{\"Pin\": \"" + pin + "\", \"Voltage\": " + String(voltage) + "}");
-            delay(100);
-        }
-        Serial.println("=============");
-
-    } else {
         digitalWrite(D3, 0);
-        digitalWrite(D4, 1);
+
+
+
+        reading = analogRead(ADC_pin);
+        Serial.print("ADC reading: ");
+        Serial.println(reading);
+
+        analog_voltage = reading * 3.3 / 1024.0;
+        Serial.print("Voltage reading: ");
+        Serial.println(analog_voltage);
+
+        Serial.println();
+
+
+
+        DisplayText("ADC reading:", 1, false, 0, 1);
+        DisplayText(String(reading), 10);
+        
+        DisplayText("Voltage:", 35, false, 0, 1);
+        DisplayText(String(analog_voltage), 45);
+        DisplayText("V", 45, false, 60);
+
+
+        if (States.MQTTConnected) {
+            d0_volt = analogRead(D0) * 3.3 / 1024.0;
+            d5_volt = analogRead(D5) * 3.3 / 1024.0;
+            d6_volt = analogRead(D6) * 3.3 / 1024.0;
+            d7_volt = analogRead(D7) * 3.3 / 1024.0;
+
+            sendMQTT("{\"Pin\": \"A0\", \"Voltage\": " + String(analog_voltage) + "}");
+            sendMQTT("{\"Pin\": \"D0\", \"Voltage\": " + String(d0_volt) + "}");
+            sendMQTT("{\"Pin\": \"D5\", \"Voltage\": " + String(d5_volt) + "}");
+            sendMQTT("{\"Pin\": \"D6\", \"Voltage\": " + String(d6_volt) + "}");
+            sendMQTT("{\"Pin\": \"D7\", \"Voltage\": " + String(d7_volt) + "}");
+        }
+
+        delay(250);
+        
+        display.clearDisplay();
+        display.display();
+    } else {
+        digitalWrite(D3, 1);
+        DisplayText("Wifi Connecting...", 0, true);
+
     }
-
-    delay(1000);
-
 }
